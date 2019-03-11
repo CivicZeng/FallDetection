b95f3e777fdf38a3a33fa0e6fa4971f643035829
CivicZeng <zzlingo49@gmail.com>
2019-03-11 10:52:00 +0800

    init

---
+++ app/src/main/java/com/example/fallover/MainActivity.java
@@ -0,0 +1,173 @@
+package com.example.fallover;
+
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Vibrator;
+import android.support.v4.content.ContextCompat;
+import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Set;
+
+public class MainActivity extends WearableActivity {
+    private static final String TAG = "MainActivity";
+    private static final String HANDLE_THREAD_NAME = "FallOver";
+    private static final long[] vibrationPattern = {0, 500, 500, 500, 500, 500, 500, 50, 250, 50, 250, 50, 500, 500, 500, 500, 500, 500, 500};
+    private static final int vibrationRepeat = -1;
+
+    private static int REQUEST_CODE = 1;
+    private static String[] permissions = new String[]{
+            Manifest.permission.VIBRATE,
+            Manifest.permission.BODY_SENSORS
+    };
+
+    private Vibrator vibrator;
+    private SensorData mSensorData;
+    private HandlerThread backgroundThread;
+    private Handler backgroundHandler;
+    private Button cancelAlert;
+
+    private void showAlertButton() {
+        final Activity activity = this;
+        activity.runOnUiThread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        cancelAlert.setBackgroundResource(R.drawable.cancel);
+//                        cancelAlert.setText(String.valueOf(mSensorData.getHeartRateValue()));
+                    }
+                });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Enables Always-on
+        setAmbientEnabled();
+
+        mSensorData = new SensorData(this);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        cancelAlert = (Button) findViewById(R.id.cancel_alert);
+        cancelAlert.setBackgroundColor(Color.BLACK);
+        cancelAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vibrator.cancel();
+                mSensorData.stopHeartReader();
+                mSensorData.init();
+                cancelAlert.setBackgroundColor(Color.BLACK);
+                cancelAlert.setText("");
+                cancelAlert.setClickable(false);
+            }
+        });
+        cancelAlert.setClickable(false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (String s : permissions) {
+                if (ContextCompat.checkSelfPermission(this, s) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(permissions, REQUEST_CODE);
+                }
+            }
+        }
+        startBackgroundThread();
+    }
+
+    @Override
+    public void onPause() {
+        stopBackgroundThread();
+        vibrator.cancel();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSensorData.destroy();
+    }
+
+    private void startBackgroundThread() {
+        backgroundThread = new HandlerThread(HANDLE_THREAD_NAME);
+        backgroundThread.start();
+        backgroundHandler = new Handler(backgroundThread.getLooper());
+        backgroundHandler.post(periodicCheck);
+    }
+
+    /**
+     * Stops the background thread and its {@link Handler}.
+     */
+    private void stopBackgroundThread() {
+        backgroundThread.quitSafely();
+        try {
+            backgroundThread.join();
+            backgroundHandler = null;
+            backgroundThread = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Runnable periodicCheck =
+            new Runnable() {
+                @Override
+                public void run() {
+                    if (!mSensorData.alert) {
+                        synchronized (this) {
+                            for (double std : mSensorData.std) {
+                                if (std > 6) {
+                                    mSensorData.alert = true;
+                                    alert();
+                                }
+                            }
+                        }
+                    }
+                    backgroundHandler.post(periodicCheck);
+                }
+            };
+
+    private void alert() {
+        //TODO 通过平台推送消息
+        Log.d(TAG, "alert");
+        cancelAlert.setClickable(true);
+        mSensorData.startHeartReader();
+        showAlertButton();
+        vibrator.vibrate(vibrationPattern, vibrationRepeat);
+        if (mSensorData.getHeartRateValue() < 40 && checkMobileConnection()) {
+            String tel = "";
+            Uri uri = Uri.parse("tel:" + tel);
+            Intent intent = new Intent(Intent.ACTION_CALL, uri);
+            startActivity(intent);
+        }
+    }
+
+    private boolean checkMobileConnection() {
+        //TODO 检测手机是否连接
+        Log.d(TAG, "checkMobileConnection");
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> bluetoothDevices = bluetoothAdapter.getBondedDevices();
+        for (BluetoothDevice device : bluetoothDevices) {
+            Log.d(TAG, device.getName());
+            Log.d(TAG, device.getAddress());
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/fallover/SensorData.java
@@ -0,0 +1,123 @@
+package com.example.fallover;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+import java.util.LinkedList;
+
+public class SensorData {
+    private static final String TAG = "FallOver";
+
+    private SensorManager sensorManager;
+    private Sensor accelSensor, gyroSensor, magnSeneor, heartRate;
+    private double[] accelValue = new double[3];
+    private double[] gyroValue = new double[3];
+    private double[] magnValue = new double[3];
+    private double heartRateValue;
+    public double[] std = {0, 0, 0};
+    public boolean alert = false;
+
+    public LinkedList<double[]> list = new LinkedList<>();
+
+    private SensorEventListener listener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            switch (event.sensor.getType()) {
+                case Sensor.TYPE_ACCELEROMETER:
+                    accelValue[0] = event.values[0];
+                    accelValue[1] = event.values[1];
+                    accelValue[2] = event.values[2];
+                    break;
+                case Sensor.TYPE_GYROSCOPE:
+                    gyroValue[0] = event.values[0];
+                    gyroValue[1] = event.values[1];
+                    gyroValue[2] = event.values[2];
+                    break;
+                case Sensor.TYPE_MAGNETIC_FIELD:
+                    magnValue[0] = event.values[0];
+                    magnValue[1] = event.values[1];
+                    magnValue[2] = event.values[2];
+                    break;
+            }
+//            String msg = accelXValue + "," + accelYValue + "," + accelZValue + ","
+//                    + gyroXValue + "," + gyroYValue + "," + gyroZValue + ","
+//                    + magnXValue + "," + magnYValue + "," + magnZValue + ","
+//                    + System.currentTimeMillis()
+//                    + "\r\n";
+            synchronized (this) {
+                list.add(new double[]{accelValue[0], accelValue[1], accelValue[2], gyroValue[0], gyroValue[1], gyroValue[2], magnValue[0], magnValue[1], magnValue[2], System.currentTimeMillis()});
+                //TODO 替换std提高精度
+                if (list.size() > 60) {
+                    for (int i = 0; i < std.length; ++i)
+                        std[i] = Math.sqrt(Math.pow(std[i], 2) + (Math.pow(gyroValue[i], 2) - Math.pow(list.remove()[3], 2)) / 60);
+                } else {
+                    for (int i = 0; i < std.length; ++i)
+                        std[i] = Math.sqrt((Math.pow(std[i], 2) * (list.size() - 1) + Math.pow(gyroValue[i], 2)) / list.size());
+                }
+            }
+            Log.d(TAG, "sensor std: " + String.valueOf(std[0]));
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+
+    private SensorEventListener heartRateListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
+                heartRateValue = event.values[0];
+                Log.d("Heart", "Heart rate: " + String.valueOf(heartRateValue));
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+
+    public void startHeartReader() {
+        Log.d("Heart", "startHeartReader");
+        sensorManager.registerListener(heartRateListener, heartRate, SensorManager.SENSOR_DELAY_UI);
+        Log.d("Heart", String.valueOf(heartRate.isWakeUpSensor()));
+    }
+
+    public void stopHeartReader() {
+        Log.d("Heart", "stopHeartReader");
+        sensorManager.unregisterListener(heartRateListener);
+        Log.d("Heart", String.valueOf(accelSensor.isWakeUpSensor()));
+    }
+
+    public double getHeartRateValue() {
+        return heartRateValue;
+    }
+
+    public void init() {
+        for (int i = 0; i < std.length; ++i)
+            std[i] = 0;
+        list.clear();
+        alert = false;
+    }
+
+    public SensorData(Context context) {
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gyroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        magnSeneor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        heartRate = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+
+        sensorManager.registerListener(listener, accelSensor, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(listener, gyroSensor, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(listener, magnSeneor, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    public void destroy() {
+        sensorManager.unregisterListener(listener);
+        sensorManager.unregisterListener(heartRateListener);
+    }
+}
